6fb7f8b17fb72a80e7384aea2865fb7665f96c7b
Gideon Isa <innocentgideon10@gmail.com>
2023-04-19 23:49:55 +0100

    initial commit

---
+++ src/main/java/com/gideon/JavagramController.java
@@ -0,0 +1,34 @@
+package com.gideon;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.validation.Valid;
+
+@Controller
+public class JavagramController {
+    
+    @GetMapping("/")
+    public String getForm(Model model) {
+        model.addAttribute("user", new User());
+        return "sign-up";
+    }
+
+    @GetMapping("/result")
+    public String getResult(Model model) {
+        //model.addAttribute(null, model)
+        return "result";
+    }
+
+    @PostMapping("/submitItem")
+    public String handleSubmit(@Valid @ModelAttribute("user") User user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "sign-up";
+        }
+        return "redirect:/result";
+    }
+}
